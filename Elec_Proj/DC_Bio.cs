a8a1c0a1e362e871a956afd748c6188925ba15f1
MH.Movasaghi <mmghho77@gmail.com>
2020-01-21 17:43:58 +0330

    Initial Project in C#

---
+++ Elec_Proj/DC_Bio.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elec_Proj
+{    
+    class DC_Bio
+    {
+        private decimal Beta { get; set; }
+
+        private decimal _V_Sat;
+        public decimal V_Sat
+        {
+            get { return _V_Sat; }
+            set {
+                if (value == 0)
+                    _V_Sat = 0.2m;
+                else
+                    _V_Sat = value;
+            }
+        }
+
+        private int Circuit_Number { get; set; }
+        private bool Je { get; set; }
+        public decimal Ie { get; set; }
+        public decimal Ib { get; set; }
+        public decimal Ic { get; set; }
+        private decimal R_C { get; set; }
+        public decimal R_B { get; set; }
+        private decimal R_B1 { get; set; }
+        private decimal R_B2 { get; set; }
+        private decimal R_E { get; set; }
+        public decimal V_BB { get; set; }
+        public decimal V_EE { get; set; }
+        private decimal V_CC { get; set; }
+
+        private decimal _V_BE;
+        public decimal V_BE
+        {
+            get { return _V_BE; }
+            set {
+                if (value == 0)
+                    _V_BE = 0.7m;
+                else
+                    _V_BE = value;
+            }
+        }
+        private decimal V_CE { get; set; }
+        public DC_Bio(bool jE, int circuit_number, decimal beta, decimal vsat = 0)
+        {
+            this.Beta = beta;
+            this.Circuit_Number = circuit_number;
+            this.Je = jE;
+            this.V_Sat = vsat;
+        }
+        public void DC_Initial(decimal rc = 0, decimal rb = 0, decimal vbb = 0, decimal vcc = 0, decimal re = 0, decimal rb1 = 0, decimal rb2 = 0,decimal vee = 0)
+        {
+            this.R_C = rc;
+            if(rb1 != 0 && rb2 != 0)
+            {                
+                this.R_B1 = rb1;
+                this.R_B2 = rb2;
+                this.R_B = (this.R_B1 * this.R_B2) / (this.R_B1 + this.R_B2);
+                this.V_BB = (this.R_B2/(this.R_B1 + this.R_B2)) * vbb;
+            }
+            else
+            {
+                this.R_B = rb;
+                this.V_BB = vbb;
+            }            
+            this.R_E = re;            
+            this.V_CC = vcc;
+            this.V_EE = vee;
+        }
+        public decimal CalCulate_VCE()
+        {
+            if (Je)
+            {
+                this.Ib = this.Cal_Ib();
+                this.Ic = this.Cal_Ic();
+                this.Ie = this.Cal_Ie();
+                this.V_CE = Cal_Vce();
+                return V_CE;
+            }
+            else
+                return 0m;
+        }        
+        private decimal Cal_Ic()
+        {
+            return Beta * Ib;
+        }
+        private decimal Cal_Ie()
+        {
+            return (Beta + 1) * Ib;
+        }
+        private decimal Cal_Ib()
+        {
+            switch (Circuit_Number)
+            {
+                case 1:
+                    return (this.V_BB - this.V_BE) / this.R_B;
+                case 2:
+                    return (this.V_BB - this.V_BE) / (this.R_B + (this.Beta + 1) * this.R_E);
+                case 3:
+                    return (this.V_CC - this.V_BE) / (this.R_B + (this.Beta + 1) * this.R_C);
+                case 4:
+                    return (this.V_CC - this.V_BE) / (this.R_B + (this.Beta + 1) * this.R_E);
+                case 5:
+                    return (this.V_BB - this.V_BE) / (this.R_B + (this.Beta + 1) * this.R_E);
+                case 6:
+                    return ((this.V_EE - this.V_BE) / (((1 / this.Beta) * this.R_B) + (((this.Beta + 1) / this.Beta) * this.R_E))) / this.Beta;
+                case 7:
+                    return (this.V_BB - this.V_BE) / (this.R_B + (this.Beta + 1) * (this.R_E + this.R_C));
+                default:
+                    return 0m;
+            }
+        }        
+        private decimal Cal_Vce()
+        {
+            switch (Circuit_Number)
+            {
+                case 1:
+                    return this.V_CC - (this.Ic * this.R_C);
+                case 2:
+                    return this.V_CC - (this.Ic * (this.R_C + ((this.Beta + 1) / this.Beta) * this.R_E));
+                case 3:
+                    return this.V_CC - ((this.Beta / (this.Beta + 1)) * (this.Ic * this.R_C));
+                case 4:
+                    return this.V_CC - (this.Ic * (this.R_C + ((this.Beta + 1) / this.Beta) * this.R_E ));
+                case 5:
+                    return this.V_CC - (this.Ic * (this.R_C + ((this.Beta + 1) / this.Beta) * this.R_E));
+                case 6:
+                    return this.V_CC + this.V_BE - (this.Ic * (this.R_C - (this.R_B / this.Beta))) ;
+                case 7:
+                    return this.V_CC - (this.Ic * ((this.Beta + 1) / this.Beta) * (this.R_C + this.R_E));
+                default:
+                    return 0m;
+            }
+        }
+        public int IsActive(decimal Vce)
+        {
+            if (Je)
+            {
+                if (Vce > this.V_Sat)
+                {
+                    return 2;
+                }
+                return 1;
+            }
+            else
+                return 0;
+        }
+        public string IsActive_Message(decimal Vce)
+        {
+            switch (this.IsActive(V_CE))
+            {
+                case 0:
+                    return "It is Disable!";
+                case 1:
+                    return "It is Saturate!";
+                case 2:
+                    return "It is Active!";
+            }
+            return "Error";
+        }
+
+    }
+}
---
+++ Elec_Proj/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elec_Proj
+{
+    class Program
+    {
+        private static string version = "1.1";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BJT DC Biasing [Version {0}]\n" +
+                "2020 Shahed University.\n" +
+                "(c) MH.Movasaghinia\n" +
+                "---------------------------------------------------------\n", Program.version);
+
+            Console.Write("Select the Circuit num. [Check the documentation] :\n" +
+                "1. Figure 1.1\n" +
+                "2. Figure 1.2\n" +
+                "3. Figure 1.3\n" +
+                "4. Figure 1.4\n" +
+                "5. Figure 1.5\n" +
+                "6. Figure 1.6\n" +
+                "7. Figure 1.7\n\n" +
+                ">>> Choise : ");
+            int circuit_num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("---------------------------------------------------------\n" +
+                              "     ********************************************        \n" +
+                              "     ####      Figure 1.{0} is Selected        ####        \n" +
+                              "     ********************************************        \n", circuit_num);
+
+            
+
+            Console.Write("Beta = ");
+            decimal Beta = decimal.Parse(Console.ReadLine());
+
+            Console.Write("V[Sat](Press 0 for default = 0.2) = ");
+            decimal V_Sat = decimal.Parse(Console.ReadLine());
+
+            //create Object
+            DC_Bio dc = new DC_Bio(true, circuit_num, Beta, V_Sat);
+
+            Console.Write("V[BE](Press 0 for default = 0.7) = ");
+            dc.V_BE = decimal.Parse(Console.ReadLine());           
+
+            Console.Write("R[C] = ");
+            decimal R_C = decimal.Parse(Console.ReadLine());
+
+            if (circuit_num == 1)
+            {
+                Console.Write("R[B] = ");
+                decimal R_B = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[BB] = ");
+                decimal V_BB = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, R_B, V_BB, V_CC);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                 ">>> I[C] = {0}\n" +
+                                 ">>> I[B] = {1}\n" +
+                                 ">>> V[CE] = {2}\n" +
+                                 ">>> {3}", dc.Ic, dc.Ib, V_CE, dc.IsActive_Message(V_CE));
+            }
+            else if (circuit_num == 2)
+            {
+                Console.Write("R[B] = ");
+                decimal R_B = decimal.Parse(Console.ReadLine());
+
+                Console.Write("R[E] = ");
+                decimal R_E = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[BB] = ");
+                decimal V_BB = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, R_B, V_BB, V_CC, R_E);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                  ">>> I[C] = {0}\n" +
+                                  ">>> I[B] = {1}\n" +
+                                  ">>> I[E] = {2}\n" +
+                                  ">>> V[CE] = {3}\n" +
+                                  ">>> {4}", dc.Ic, dc.Ib, dc.Ie, V_CE, dc.IsActive_Message(V_CE));
+            }
+            else if (circuit_num == 3)
+            {
+                Console.Write("R[B] = ");
+                decimal R_B = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, R_B, 0, V_CC);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                  ">>> V[BB] = {0}\n" + 
+                                  ">>> I[C] = {1}\n" +
+                                  ">>> I[B] = {2}\n" +
+                                  ">>> V[CE] = {3}\n" +
+                                  ">>> {4}", dc.V_BB, dc.Ic, dc.Ib, V_CE, dc.IsActive_Message(V_CE));
+            }
+            else if (circuit_num == 4)
+            {
+                Console.Write("R[B] = ");
+                decimal R_B = decimal.Parse(Console.ReadLine());
+
+                Console.Write("R[E] = ");
+                decimal R_E = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, R_B, 0, V_CC, R_E);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                  ">>> V[BB] = {0}\n" +
+                                  ">>> I[C] = {1}\n" +
+                                  ">>> I[B] = {2}\n" +
+                                  ">>> I[E] = {3}\n" +
+                                  ">>> V[CE] = {4}\n" +
+                                  ">>> {5}", dc.V_BB, dc.Ic, dc.Ib, dc.Ie, V_CE, dc.IsActive_Message(V_CE));
+            }
+            else if (circuit_num == 5)
+            {
+                Console.Write("R[B1] = ");
+                decimal R_B1 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("R[B2] = ");
+                decimal R_B2 = decimal.Parse(Console.ReadLine());
+
+                Console.Write("R[E] = ");
+                decimal R_E = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, 0, 0, V_CC, R_E, R_B1, R_B2);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                  ">>> V[th] = {0}\n" +
+                                  ">>> R[th] = {1}\n" +
+                                  ">>> I[C] = {2}\n" +
+                                  ">>> I[B] = {3}\n" +
+                                  ">>> I[E] = {4}\n" +
+                                  ">>> V[CE] = {5}\n" +
+                                  ">>> {6}", dc.V_BB, dc.R_B, dc.Ic, dc.Ib, dc.Ie, V_CE, dc.IsActive_Message(V_CE));
+            }
+            else if (circuit_num == 6)
+            {
+                Console.Write("R[B] = ");
+                decimal R_B = decimal.Parse(Console.ReadLine());
+
+                Console.Write("R[E] = ");
+                decimal R_E = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[EE] = ");
+                decimal V_EE = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, R_B, 0, V_CC, R_E, 0, 0, V_EE);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                  ">>> V[BB] = {0}\n" +
+                                  ">>> V[EE] = {1}\n" +
+                                  ">>> I[C] = {2}\n" +
+                                  ">>> I[B] = {3}\n" +
+                                  ">>> I[E] = {4}\n" +
+                                  ">>> V[CE] = {5}\n" +
+                                  ">>> {6}", dc.V_BB, dc.V_EE, dc.Ic, dc.Ib, dc.Ie, V_CE, dc.IsActive_Message(V_CE));
+            }
+            else if (circuit_num == 7)
+            {
+                Console.Write("R[B] = ");
+                decimal R_B = decimal.Parse(Console.ReadLine());
+
+                Console.Write("R[E] = ");
+                decimal R_E = decimal.Parse(Console.ReadLine());
+
+                Console.Write("V[CC] = ");
+                decimal V_CC = decimal.Parse(Console.ReadLine());
+
+                dc.DC_Initial(R_C, R_B, 0, V_CC, R_E);
+
+                decimal V_CE = dc.CalCulate_VCE();
+
+                Console.WriteLine("-----------------------------------");
+
+                Console.WriteLine("## Result:\n" +
+                                  ">>> V[BB] = {0}\n" +
+                                  ">>> I[C] = {1}\n" +
+                                  ">>> I[B] = {2}\n" +
+                                  ">>> I[E] = {3}\n" +
+                                  ">>> V[CE] = {4}\n" +
+                                  ">>> {5}", dc.V_BB, dc.Ic, dc.Ib, dc.Ie, V_CE, dc.IsActive_Message(V_CE));
+            }
+
+        }
+    }
+}
